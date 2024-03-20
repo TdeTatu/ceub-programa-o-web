c2c82f307cba1d908fd0bab0bcb9b9408acdbddb
TdeTatu <jonatasluizgs@gmail.com>
2024-03-20 08:47:48 -0300

    adds js pra numericos

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 console.log("Hello World")
 
-let teste;
+let teste = 1.1;
+let variavelString = "Olá mundo!";
+let aceitoCookies = false;
+
+const PI= 3.14;
 
 console.log(teste)
+console.log(variavelString)
+
+aceitoCookies = true;
+
+teste = "abc";
+
+console.log(typeof(teste))
+
+console.log(typeof(aceitoCookies))
+
+console.log(5+5)
